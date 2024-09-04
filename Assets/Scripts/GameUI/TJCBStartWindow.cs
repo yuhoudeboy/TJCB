7def5147bb5124fef856a4f670e3f0bfaddf4a10
yuhoudeboy <zhoupeng015@gmail.com>
2024-09-04 19:55:51 +0800

    第二次提交,资源加载简单实现，和登录按钮显示

--- Assets/Scripts/GameUI/TJCBStartWindow.cs
+++ Assets/Scripts/GameUI/TJCBStartWindow.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class TJCBStartWindow : MonoBehaviour
 {
     [SerializeField]
+    private GameObject m_LoadInitResourceObj;
+
+    [SerializeField]
     private Image m_ProgressBarImg;
 
     [SerializeField]
@@ -13,15 +16,61 @@ public class TJCBStartWindow : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField]
-    private Button PlatformLoginBt;
+    private Button m_PlatformLoginBt;
     void Start()
     {
-        
+        LoadInitResource();
+    }
+
+
+    private void LoadInitResource() 
+    {
+        m_LoadInitResourceObj.SetActive(true);
+
+        m_PlatformLoginBt.gameObject.SetActive(false);
+
+        m_ProgressBarImg.fillAmount = 0.0f;
+
+        m_ProgressTxt.text = "0.00";
+
+        StartCoroutine(LoadInitResourceSimulation());
+    }
+
+    private IEnumerator LoadInitResourceSimulation() 
+    {
+        float totalTime = 0f;
+        while (totalTime < 5f)
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            totalTime += 0.5f;
+
+            m_ProgressBarImg.fillAmount += 0.1f;
+
+            m_ProgressTxt.text = string.Format
+                ("{0:P}", m_ProgressBarImg.fillAmount) +  " %";
+        }
+
+        yield return new WaitForSeconds(0.3f);
+        LoadInitResourceFinish();
+
+        yield break;
+
+    }
+
+    private void LoadInitResourceFinish() 
+    {
+        m_LoadInitResourceObj.SetActive(false);
+
+        m_PlatformLoginBt.gameObject.SetActive(true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
 }
