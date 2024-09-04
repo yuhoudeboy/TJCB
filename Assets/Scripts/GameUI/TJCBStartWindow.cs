6b516d0e6e0e0bc8914b843d8b305b92a768e71e
yuhoudeboy <zhoupeng015@gmail.com>
2024-09-04 16:39:21 +0800

    淘金城堡，UI绘制，游戏加载条，和登录按钮

---
+++ Assets/Scripts/GameUI/TJCBStartWindow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TJCBStartWindow : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_ProgressBarImg;
+
+    [SerializeField]
+    private Text m_ProgressTxt;
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private Button PlatformLoginBt;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
